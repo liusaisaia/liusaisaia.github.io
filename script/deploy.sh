ec5d22e952e40bedf50f3145d91bb748fbc84cb8
liusaisai <56034541+liusaisaia@users.noreply.github.com>
2023-08-01 03:35:46 +0000

    配置静态站点

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,21 +3,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 重新打包组件库
-
-pnpm build
-
 # 打包生成静态文件
 pnpm docs:build
 
 # 进入待发布的 dist/ 目录
 cd docs/.vitepress/dist
 
+# 提交打包静态网站到 github-pages 分支
 git init
 git add .
 git commit -m 'deploy'
 
-# 部署到 https://.github.io/
+# 部署到 https://<username>.github.io/<repo>
 git push -f git@github.com:liusaisaia/vitepressBlog.git master:gh-pages
 
 # 提交所有代码到github
@@ -25,3 +22,4 @@ cd ../../../
 git add .
 git cm -m 'update'
 git push
+
