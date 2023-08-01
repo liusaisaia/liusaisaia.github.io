3c2c377b89c9b25c4857666d079de0c307c2393e
liusaisai <56034541+liusaisaia@users.noreply.github.com>
2023-08-01 03:11:12 +0000

    增加打包

---
+++ script/deploy.sh
@@ -0,0 +1,27 @@
+# /bin/bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 重新打包组件库
+
+pnpm build
+
+# 打包生成静态文件
+pnpm docs:build
+
+# 进入待发布的 dist/ 目录
+cd docs/.vitepress/dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+# 部署到 https://.github.io/
+git push -f git@github.com:liusaisaia/vitepressBlog.git master:gh-pages
+
+# 提交所有代码到github
+cd ../../../
+git add .
+git cm -m 'update'
+git push
