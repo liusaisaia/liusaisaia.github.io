fd822a12088286f10f4d69fecc137b68941b1e49
liusaisai <56034541+liusaisaia@users.noreply.github.com>
2023-09-10 18:15:57 +0800

    feat(更改配置): 修改

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,11 @@
 # /bin/bash
+###
+ # @Author: Liu SaiSai
+ # @Date: 2023-08-03 18:03:44
+ # @LastEditors: Liu SaiSai
+ # @FilePath: \vitepress-docs\script\deploy.sh
+ # @Description: Do not edit
+### 
 
 
 # 确保脚本抛出遇到的错误
@@ -12,7 +19,7 @@ cd docs/.vitepress/dist
 
 # 提交打包静态网站到 github-pages 分支
 git init
-git add .
+git add -A
 git commit -m 'deploy'
 
 # 部署到 https://<username>.github.io/<repo> 不成功
